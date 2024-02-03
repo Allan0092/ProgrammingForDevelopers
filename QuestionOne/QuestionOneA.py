379609f80969feaa91cc57f0bcd94a1a914c1d79
Allan <allan.gtm92@gmail.com>
2024-02-03 10:00:49 +0545

    list based on the length of cost is created

--- QuestionOne/QuestionOneA.py
+++ QuestionOne/QuestionOneA.py
@@ -10,20 +10,34 @@ Find minimum Cost
 """
 
 
-def createCostTable(cost):
-    sum_table = [[0,0,0],[0,0,0], [0,0,0]]
-    for n in range(len(cost[0])):
-        for k in range(len(cost[1])):
-            if n==k:
-                sum_table[n][k]=-1
+def createCostTable(costs):
+    '''
+        This function creates a [k][k] length of list of the sum of two theme of two venues. The value for the same theme is -1.
+            Parameters:
+                costs (int[n:Venue][k:theme]): cost of decorating the venue.
+        
+            Returns:
+                int[k][k] : sum of all the possible cost between two venues.
+    '''
+    # sum_table = [[0,0,0],[0,0,0], [0,0,0]]
+    sum_table = []
+    for i in range(len(costs[0])):
+        sum_table.append([])
+        for j in range(len(costs[1])):
+            sum_table[i].append(None)
+    
+    for i in range(len(costs[0])):
+        for j in range(len(costs[1])):
+            if i==j:
+                sum_table[i][j]=-1
                 continue
-            sum_table[n][k]=cost[0][k]+cost[1][n] 
+            sum_table[i][j]=costs[0][i]+costs[1][j] 
 
     return sum_table
 
 
 def main():
-    cost=[[1,5,3],[2,9,4]]
+    cost=[[7, 2, 3], [1, 5, 6,7]]
     print(createCostTable(cost))
     
 
